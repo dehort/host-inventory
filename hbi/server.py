1c8f7a0f35cce735b73e1329e95f01b65697d1f7
Kyle Lape <klape@redhat.com>
2018-08-16 10:39:16 -0500

    Lots of refactoring; Basic create/update; set IDs

--- hbi/server.py
+++ hbi/server.py
@@ -1,17 +1,129 @@
-from concurrent import futures
-
 import time
 import grpc
+import uuid
 
-from hbi import hbi_pb2_grpc
+from hbi import hbi_pb2_grpc, hbi_pb2
+from concurrent import futures
+from collections import defaultdict
+
+
+class HostAdapter(object):
+
+    def __init__(self, host):
+        self.host = host
+        self.id = host.id
+        self.canonical_facts = {f.key: f.value for f in host.canonical_facts}
+        self.facts = self._adapt_facts()
+
+    def _adapt_facts(self):
+        d = defaultdict(dict)
+        for fact in self.host.facts:
+            d[fact.namespace][fact.key] = fact.value
+        return d
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def to_host(self):
+        facts = [hbi_pb2.Fact(namespace=namespace, key=k, value=v)
+                 for namespace, facts in self.facts.items()
+                 for k, v in facts.items()]
+
+        canonical_facts = [hbi_pb2.CanonicalFact(key=k, value=v)
+                           for k, v in self.canonical_facts.items()]
+
+        return hbi_pb2.Host(id=self.id, display_name=self.host.display_name,
+                            canonical_facts=canonical_facts,
+                            facts=facts)
+
+    def merge(self, new):
+        for k, v in new.canonical_facts.items():
+            self.canonical_facts[k] = v
+
+        for namespace, d in new.facts.items():
+            self.facts[namespace] = d
+
+
+class Index(object):
+
+    def __init__(self):
+        self.dict_ = {}
+
+    def add(self, host_adapter):
+        self.dict_[host_adapter.id] = host_adapter
+        for t in host_adapter.canonical_facts.items():
+            self.dict_[t] = host_adapter
+
+    def get(self, host_adapter):
+        if host_adapter.id:
+            h = self.dict_.get(host_adapter.id)
+            if h:
+                return h
+            raise ValueError(f"Could not locate a host with given id {host_adapter.id}")
+
+        for t in host_adapter.canonical_facts.items():
+            h = self.dict_.get(t)
+            if h:
+                return h
+
+    def merge(self, orig, new):
+        for t in orig.canonical_facts.items():
+            del self.dict_[t]
+
+        orig.merge(new)
+
+        for t in orig.canonical_facts.items():
+            self.dict_[t] = orig
 
 
 class Servicer(hbi_pb2_grpc.HostInventoryServicer):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.index = Index()
+        self.host_keys = defaultdict(set)
+
     def CreateOrUpdate(self, host_list, context):
+        new_host_list = []
         for host in host_list.hosts:
-            print(host)
-        return host_list
+            if host.canonical_facts is None and host.id is None:
+                raise ValueError("Must provide at least one canonical fact or the ID")
+
+            h = HostAdapter(host)
+
+            # search the canonical_facts dict for a match
+            existing_host = self.index.get(h)
+            if existing_host:
+                self.index.merge(existing_host, h)
+            else:  # Host not found.  Create it.
+                existing_host = h
+                existing_host.id = uuid.uuid4().hex
+                self.index.add(h)
+
+            new_host_list.append(existing_host.to_host())
+
+        return hbi_pb2.HostList(hosts=new_host_list)
+
+    def Get(self, host_list, context):
+        result = hbi_pb2.HostList()
+
+        for host in host_list.hosts:
+            match = None
+            for fact in host.canonical_facts:
+                fact_string = "=".join(fact.key, fact.value)
+                m = self.canonical_facts.get(fact_string)
+
+                if m is None or m != match:
+                    match = None
+                    break
+
+                match = m
+
+            if match:
+                result.hosts.append(match)
 
 
 def serve():
--- hbi/tests.py
+++ hbi/tests.py
@@ -1,21 +1,49 @@
-from hbi.server import Servicer
-from hbi.hbi_pb2 import Host, HostList, Fact
+from hbi.server import Servicer, HostAdapter
+from hbi.hbi_pb2 import Host, HostList, CanonicalFact
 from hbi.util import names
 
 
-def test_server():
-    server = Servicer()
+HOST_LIST = HostList(hosts=[
+    Host(
+        display_name="-".join(display_name),
+        canonical_facts=[
+            CanonicalFact(
+                key="hostname",
+                value=f"{'-'.join(display_name)}.com",
+            )
+        ]) for display_name in names()
+])
 
-    host_list = HostList(hosts=[
-        Host(
-            display_name="-".join(display_name),
-            facts=[
-                Fact(
-                    namespace="demo",
-                    key="hostname",
-                    value=f"{'-'.join(display_name)}.com",
-                )
-            ]) for display_name in names()
-    ])
 
-    assert host_list == server.CreateOrUpdate(host_list, None)
+def test_create():
+    ret_hosts = [HostAdapter(h) for h in Servicer().CreateOrUpdate(HOST_LIST, None).hosts]
+    ret_hostnames = set(h.canonical_facts["hostname"] for h in ret_hosts)
+    original_hosts = [HostAdapter(h) for h in HOST_LIST.hosts]
+    original_hostnames = set(h.canonical_facts["hostname"] for h in original_hosts)
+    assert ret_hostnames == original_hostnames
+
+
+def gen_host(canonical_facts):
+    facts = [CanonicalFact(key=k, value=v) for k, v in canonical_facts.items()]
+    return Host(canonical_facts=facts)
+
+
+def test_update():
+    service = Servicer()
+    canonical_facts = {
+        "insights_id": "1234",
+        "hostname": "inventory-test.redhat.com"
+    }
+    host = gen_host(canonical_facts)
+    service.CreateOrUpdate(HostList(hosts=[host]), None)
+    del canonical_facts["insights_id"]
+    host = gen_host(canonical_facts)
+    host.facts.add(key="cpu.count", value="4")
+    ret = service.CreateOrUpdate(HostList(hosts=[host]), None)
+    assert ret.hosts[0].facts[0].value == "4"
+    for k, v in ((f.key, f.value) for f in ret.hosts[0].canonical_facts):
+        print(k, v)
+        if k == "insights_id":
+            assert v == "1234"
+            return
+    assert False, "Failed to find insights_id"
