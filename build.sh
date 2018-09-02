a80053c66fca5526915f03cda418b10d15017e90
Kyle Lape <klape@redhat.com>
2018-09-02 16:09:01 -0500

    Make build.sh work on os x

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 python3 -m grpc_tools.protoc -I proto --python_out=hbi --grpc_python_out=hbi proto/hbi.proto
-sed -i 's/import hbi/from . import hbi/' hbi/hbi_pb2_grpc.py
+sed -i -e 's/import hbi/from . import hbi/' hbi/hbi_pb2_grpc.py
